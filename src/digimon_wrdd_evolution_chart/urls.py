da486bd74f9d97826ca0e581dca01e12f6194d01
Verrya <63302524+Verrya@users.noreply.github.com>
2023-12-03 18:00:22 +0100

    create a test route

--- src/digimon_wrdd_evolution_chart/urls.py
+++ src/digimon_wrdd_evolution_chart/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .views import *
+from django.urls import include, path
 
 urlpatterns = [
+    path('', include("evolution_chart.urls"), name='evolution_chart-index'),
     path('admin/', admin.site.urls),
 ]
