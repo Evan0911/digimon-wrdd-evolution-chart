5cd29f63b3995f3160ec2d5ccd2de5412292795a
Verrya <63302524+Verrya@users.noreply.github.com>
2023-12-05 16:41:39 +0100

    create digimonData class and a dummy digimon to print its data within the component

--- src/components/digimon_block/digimon_block.py
+++ src/components/digimon_block/digimon_block.py
@@ -1,10 +1,18 @@
 from django_components import component
+from interfaces.digimon_data import digimonData
 
 @component.register("digimonBlock")
 class DigimonBlock(component.Component):
     # Templates inside `[your apps]/components` dir and `[project root]/components` dir will be automatically found. To customize which template to use based on context
     # you can override def get_template_name() instead of specifying the below variable.
     template_name = "digimon_block/digimon_block.html"
+    
+    
+    def get_context_data(self):
+      return {
+        "tailmon": digimonData("Gatomon", "Tailmon", ["Angewomon", "Holydramon"], ["Plotmon"], 0, 1000, 0, 80, 0, 60, 10, 0, 0, 1, 0, 0, 0, 0, 15, 0, 14, 7, 3)
+
+      }
 
     class Media:
         css = "digimon_block/digimon_block.css"
---
+++ src/interfaces/digimon_data.py
@@ -0,0 +1,25 @@
+class digimonData:
+    def __init__(self, occidentalName, japaneseName, evolutions, preEvolutions, requiredHp, requiredMp, requiredAtk, requiredBrain, requiredDef, requiredSpd, minimumWeight, maximumWeight, minimumCareMistake, maximumCareMistake, minimumHappiness, maximumHappiness, minimumDiscipline, maximumDiscipline, minimumBattle, maximumBattle, requiredTechs, requiredDecode, quota):
+        self.occidentalName = occidentalName
+        self.japaneseName = japaneseName
+        self.evolutions = evolutions
+        self.preEvolutions = preEvolutions
+        self.requiredHp = requiredHp
+        self.requiredMp = requiredMp
+        self.requiredAtk = requiredAtk
+        self.requiredDef = requiredDef
+        self.requiredSpd = requiredSpd
+        self.requiredBrain = requiredBrain
+        self.minimumWeight = minimumWeight
+        self.maximumWeight = maximumWeight
+        self.minimumCareMistake = minimumCareMistake
+        self.maximumCareMistake = maximumCareMistake
+        self.minimumHappiness = minimumHappiness
+        self.maximumHappiness = maximumHappiness
+        self.minimumDiscipline = minimumDiscipline
+        self.maximumDiscipline = maximumDiscipline
+        self.minimumBattle = minimumBattle
+        self.maximumBattle = maximumBattle
+        self.requiredTechs = requiredTechs
+        self.requiredDecode = requiredDecode
+        self.quota = quota
